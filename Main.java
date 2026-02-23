72d4ded76a9cd69257f54ac18df863818338d337
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-02-23 13:04:36 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         int a = 121;
+//         int original = a;
+//         int reversed = 0;
+
+//         while (a > 0) {
+//             int digit = a % 10;
+//             reversed = reversed * 10 + digit;
+//             a = a / 10;
+//         }
+
+//         if (original == reversed) {
+//             System.out.println("Palindrome");
+//         } else {
+//             System.out.println("Not Palindrome");
+//         }
+//     }
+// }
+
+public class Main {
+    public static void main(String[] args) {
+        // byte a= 4;
+        // byte b = 5;
+        // byte sum = (byte) (a + b);
+        // System.out.println(sum);
+
+        // byte a = 4;
+        // byte b = (byte) (a);
+        // System.out.println(b); 
+        int a = 4;
+        byte b = 3;
+        byte sum = (byte) (a + b);
+        System.out.println(sum);
+    }
+}
