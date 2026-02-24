2fb8bb1d9215525eb5b5e05d2a2815994ef5b015
AbhirajSinghRajpoot <Abhirajsingh2k5@gmail.com>
2026-02-24 14:10:19 +0530

    Refactor Main.java to comment out unique character logic and implement frequency count for characters in a string

--- Main.java
+++ Main.java
@@ -82,15 +82,26 @@ public class Main {
         // } else {
         //     System.out.println("Not Palindrome");
         // }
-        String result = "";
+        // String result = "";
 
-        for (int i = 0; i < a.length(); i++) {
-            char current = a.charAt(i);
-            if (result.indexOf(current) == -1) {
-                result += current;
+        // for (int i = 0; i < a.length(); i++) {
+        //     char current = a.charAt(i);
+        //     if (result.indexOf(current) == -1) {
+        //         result += current;
+        //     }
+        // }
+        // System.out.println(result);
+        String frequency = "hello world";
+        for (int i = 0; i < frequency.length(); i++) {
+            char current = frequency.charAt(i);
+            int count = 0;
+            for (int j = 0; j < frequency.length(); j++) {
+                if (frequency.charAt(j) == current) {
+                    count++;
+                }
             }
+            System.out.println(current + ": " + count);
         }
-        System.out.println(result);
     }
 }
 
